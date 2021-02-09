eb5909d4162717549417719461c1ada1efdfa674
Adriana Lazurca <adriana.lazurca@gmail.com>
2021-02-10 00:53:23 +0100

    add last date ex

--- 2.The-Hill/1.Javascript/exercises/04-dates/04-locale-date/script.js
+++ 2.The-Hill/1.Javascript/exercises/04-dates/04-locale-date/script.js
@@ -10,6 +10,20 @@
 // You will have time to focus on it later.
 
 (() => {
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-    // your code here
+  // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+  // your code here
+
+  var today = new Date();
+  var fullDate = today.toLocaleDateString("fr-FR", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  var hours = today.getHours();
+  var min = today.getMinutes();
+  var fullTime = hours + "h" + min;
+  var todayFr = fullDate + ", " + fullTime;
+  var element = document.getElementById("target");
+  element.innerHTML = todayFr;
 })();
